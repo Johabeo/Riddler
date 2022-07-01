42abac9c7b5a80ef458648751483c9c69c331bb8
David Odhiambo <odhiambodave@live.com>
2022-07-01 10:13:47 -0700

    Add Test Data and more tests

--- app/src/test/java/com/example/riddler/data/database/AppDatabaseTest.kt
+++ app/src/test/java/com/example/riddler/data/database/AppDatabaseTest.kt
@@ -1,6 +1,10 @@
 package com.example.riddler.data.database
 
+import android.app.Application
 import com.example.riddler.data.dao.QuizDao
+import com.example.riddler.data.di.AppModule
+import com.example.riddler.data.model.Questions
+import com.example.riddler.data.model.Quiz
 import io.mockk.*
 import io.mockk.impl.annotations.RelaxedMockK
 import org.junit.Assert.*
@@ -15,7 +19,17 @@ class AppDatabaseTest {
 
     @Test
     fun quizDao() {
-        every { dao.getQuizQuestions(1) } just Runs
+        val database = mockk<AppDatabase>()
+        val dao = mockk<QuizDao>()
+        val application = mockk<Application>()
+        val appModule = mockk<AppModule>()
+    //quiz
+        val quiz1 = Quiz("Joe","Best Quize","people Around the World","General",23)
+    //Questions
+        val questions1 : Questions = Questions(1,"What's your group", "G1","G2","G3","G4","G1")
+
+
+        every { dao.getQuizQuestions(23) } returns listOf(questions1)
 
     }
 }
