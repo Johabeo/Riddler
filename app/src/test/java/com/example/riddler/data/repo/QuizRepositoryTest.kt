ca76679fddae97a87b8a449d0fa4dd73916b7d45
David Odhiambo <odhiambodave@live.com>
2022-07-02 11:05:12 -0700

    Fixed tests in the QuizRepositoryTest adding assertions and test data

--- app/src/test/java/com/example/riddler/data/repo/QuizRepositoryTest.kt
+++ app/src/test/java/com/example/riddler/data/repo/QuizRepositoryTest.kt
@@ -17,14 +17,48 @@ class QuizRepositoryTest() {
     val underTest = QuizRepository(
         dao = dao,
     )
+    var fakeList :List<Quiz> = (listOf<Quiz>(
+        Quiz("G1","fromtest","","",1)
+
+    ))
+    val fakelist2 : List<Questions> = (listOf<Questions>(
+        Questions(1,"What's your group", "G1"
+            ,"G2","G3","G4","G1")
+    ))
+
+    var observableFakelist = Observable.fromArray((fakeList))
+
+    val myQuiz:Quiz = Quiz("G1","Our Team","test1",
+        "test to insert quiz",1)
+    val myFavQuiz = FavoriteQuiz(
+        1,1,1)
+    val questions = Questions(
+        1,"whats your group","G1"
+        ,"G2","G3","Dont know"
+        ,"G1",1)
 
     @Test
+    /*getFavoriteQuizzes(user: Int): Observable<List<Quiz>> {
+        return dao.getFavoriteQuizzes(user)*/
     fun getFavoriteQuizzes(){
         val dao = mockk<QuizDao>()
         val underTest = QuizRepository(
             dao = dao,
         )
-        coEvery {  underTest.getFavoriteQuizzes(1) } returnsMany listOf()
+        coEvery {  underTest.getFavoriteQuizzes(1) } returns observableFakelist
+        assertEquals(observableFakelist,underTest.getFavoriteQuizzes(1))
+
+    }
+    @Test
+    /*getMyQuizzes(user: Int): Observable<List<Quiz>> {
+        return dao.getMyQuizzes(user)*/
+    fun getMyQuizzes(){
+        val dao = mockk<QuizDao>()
+        val underTest = QuizRepository(
+            dao = dao,
+        )
+        coEvery {  underTest.getMyQuizzes(1) }returns observableFakelist
+        assertEquals(observableFakelist,underTest.getMyQuizzes(1))
 
     }
 
@@ -34,49 +68,62 @@ class QuizRepositoryTest() {
         val underTest = QuizRepository(
             dao = dao,
         )
-        every { underTest.getQuizQuestion(1) } just Runs
+        val fakelist2 : List<Questions> = (listOf<Questions>(Questions(1,"What's your group", "G1","G2","G3","G4","G1")
+        ))
+
+        every { underTest.getQuizQuestion(1) } returns fakelist2
+
+        assertEquals(fakelist2,underTest.getQuizQuestion(1) )
     }
 
     @Test
-    fun insertQuiz() {
+    /*insertQuiz(quiz: Quiz): Long {
+        return dao.insertQuiz(quiz)*/
+    fun test_for_insertQuiz() {
+        val dao = mockk<QuizDao>()
+
+        val underTest = QuizRepository(
+            dao = dao,
+        )
+
+        every { underTest.insertQuiz(myQuiz) } returns 0
+        assertEquals(0,underTest.insertQuiz(myQuiz))
+
+    }
+    @Test
+    //insertFavoriteQuiz(myFavQuiz)
+    fun test_for_insertFavoriteQuiz() {
         val dao = mockk<QuizDao>()
         val underTest = QuizRepository(
             dao = dao,
         )
-       every { underTest.insertQuiz(quiz = Quiz("G1","Our Team","test1",
-           "test to insert quiz",1)) } just Runs
-    }
+        every { underTest.insertFavoriteQuiz(myFavQuiz) } returns Unit
+        dao.insertFavoriteQuiz(myFavQuiz)
+        assertEquals(Unit,underTest.insertFavoriteQuiz(myFavQuiz))
 
+    }
     @Test
-    fun insertFavoriteQuiz() {
+    //insertQuestions(questions: Questions)
+    fun test_for_insertQuestions() {
         val dao = mockk<QuizDao>()
         val underTest = QuizRepository(
             dao = dao,
         )
-
-        every { underTest.insertFavoriteQuiz(FavoriteQuiz(
-            1,1,1))}returns Unit
-
-    }
-
-    @Test
-    fun insertQuestions() {
-        val dao = mockk<QuizDao>()
-        val underTest = QuizRepository(
-            dao = dao,
-        )
-        every { underTest.insertQuestions(questions = Questions(
+        val questions = Questions(
             1,"whats your group","G1"
             ,"G2","G3","Dont know"
-            ,"G1",1)) } returns Unit
-        every { underTest.insertQuestions(questions = Questions(
-            1,"whats your group","G1"
-            ,"G2","G3","Dont know"
-            ,"G1",1)) }
+            ,"G1",1)
+
+        every { underTest.insertQuestions(questions) } returns 0
+        assertEquals(0,underTest.insertQuestions(questions))
 
     }
 }
 
-private infix fun <T, B> MockKStubScope<T, B>.just(runs: Runs) {
+private infix fun <T, B> MockKStubScope<T, B>.returns(myFunc: suspend (FavoriteQuiz) -> B) {}
 
-}
+private infix fun <T, B> MockKStubScope<T, B>.returns(i: Int) {}
+
+private infix fun <T, B> MockKStubScope<T, B>.returns(unit: Unit) {}
+
+private infix fun <T, B> MockKStubScope<T, B>.just(runs: Runs) {}
