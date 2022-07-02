153ed47cdf81fb6683ce3565f0d40bdbdc8ca8fc
David Odhiambo <odhiambodave@live.com>
2022-07-02 11:28:42 -0700

    updated assertions tests clean up code

--- app/src/test/java/com/example/riddler/data/di/AppModuleTest.kt
+++ app/src/test/java/com/example/riddler/data/di/AppModuleTest.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import com.example.riddler.RetroApiInterface
 import com.example.riddler.data.dao.QuizDao
 import com.example.riddler.data.database.AppDatabase
+import com.example.riddler.data.repo.FirestoreRepository
 import com.example.riddler.ui.viewmodel.just
 import io.mockk.*
 import org.junit.Assert.*
@@ -14,48 +15,55 @@ import org.junit.Test
 
 class AppModuleTest {
     val appModule = mockk<AppModule>()
-
-
     @Before
     fun setUp() {
         val dao = mockk<QuizDao>()
         val api = mockk<RetroApiInterface>()
         val database = mockk<AppDatabase>()
-
-
     }
-
     @After
     fun tearDown() {
         unmockkAll()
     }
-
     @Test
-    //getAppDB()
-    fun getAppDB() {
+    //getAppDB(context:Application):AppDatabase
+    fun test_function_getAppDB() {
         val dao = mockk<QuizDao>()
         val api = mockk<RetroApiInterface>()
         val database = mockk<AppDatabase>()
         val application = mockk<Application>()
 
         coEvery { appModule.getAppDB(application) } returns database
+        val result = appModule.getAppDB(application)
+        assertEquals(database, result)
+
     }
 
     @Test
-    //getQuizDao()
-    fun getQuizDao() {
+    //getQuizDao(appDB:AppDatabase):QuizDao
+    fun test_function_getQuizDao() {
         val database = mockk<AppDatabase>()
         val dao = mockk<QuizDao>()
         coEvery { appModule.getQuizDao(database) } returns dao
-
+        val result = appModule.getQuizDao(database)
+        assertEquals(dao, result)
     }
 
     @Test
-    //getRetroApi()
-    fun getRetroApi() {
+    //getRetroApi():RetroApiInterface
+    fun test_for_getRetroApi() {
         val api = mockk<RetroApiInterface>()
-        //verify { appModule.getRetroApi() }
         coEvery { appModule.getRetroApi() } returns api
+        val result = appModule.getRetroApi()
+        assertEquals(api, result)
+    }
+    @Test
+    //getFirestoreRepository():FirestoreRepository
+    fun test_for_getFirestoreRepository(){
+        val repo = mockk<FirestoreRepository>()
+        coEvery { appModule.getFirestoreRepository() } returns repo
+        val result = appModule.getFirestoreRepository()
+        assertEquals(repo, result)
 
     }
 }
