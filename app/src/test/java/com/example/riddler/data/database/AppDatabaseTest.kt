76dcf61eafbc161eadf448acdd82d6c1c60d0ed4
David Odhiambo <odhiambodave@live.com>
2022-07-03 19:23:12 -0700

    redo appdatabaseTest and AppModuleTest

--- app/src/test/java/com/example/riddler/data/database/AppDatabaseTest.kt
+++ app/src/test/java/com/example/riddler/data/database/AppDatabaseTest.kt
@@ -18,7 +18,7 @@ class AppDatabaseTest {
 
 
     @Test
-    fun quizDao() {
+    fun `test that quizDao function returns dao`() {
         val database = mockk<AppDatabase>()
         val dao = mockk<QuizDao>()
         val application = mockk<Application>()
@@ -29,7 +29,93 @@ class AppDatabaseTest {
         val questions1 : Questions = Questions(1,"What's your group", "G1","G2","G3","G4","G1")
 
 
-        every { dao.getQuizQuestions(23) } returns listOf(questions1)
+        every { database.quizDao() } returns dao
+        assertEquals(dao, database.quizDao())
+    }
+
+       @Test
+       fun `test that insertQuiz function inserts quiz and returns Long`() {
+           val database = mockk<AppDatabase>()
+           val dao = mockk<QuizDao>()
+           val application = mockk<Application>()
+           val appModule = mockk<AppModule>()
+
+         //quiz
+              val quiz1 = Quiz("Joe","Best Quize","people Around the World","General",23)
+            //Questions
+                val questions1 : Questions = Questions(1,"What's your group", "G1","G2","G3","G4","G1")
+           every { dao.insertQuiz(quiz1) }returns 0 //inserts quiz
+              every { database.quizDao() } returns dao
+                assertEquals(0, database.quizDao().insertQuiz(quiz1))
+         }
+
+
+            @Test
+            fun `test that getQuizzes function returns quiz`() {
+                val database = mockk<AppDatabase>()
+                val dao = mockk<QuizDao>()
+                val application = mockk<Application>()
+                val appModule = mockk<AppModule>()
+              //quiz
+                 val quiz1 = Quiz("Joe","Best Quize","people Around the World","General",23)
+               //Questions
+                //Observable<List<Quiz>>
+
+                   val questions1 : Questions = Questions(1,"What's your group", "G1","G2","G3","G4","G1")
+                every { dao.getQuizzes(1,0) }returns dao.getQuizzes(1,0)
+                   assertEquals(dao.getQuizzes(1,0 ), dao.getQuizzes(1,0))
+            }
+                @Test
+                fun `test that getQuizById function returns quiz`() {
+                    val database = mockk<AppDatabase>()
+                    val dao = mockk<QuizDao>()
+                    val application = mockk<Application>()
+                    val appModule = mockk<AppModule>()
+                  //quiz
+                     val quiz1 = Quiz("Joe","Best Quize","people Around the World","General",23)
+                   //Questions
+                       val questions1 : Questions = Questions(1,"What's your group", "G1","G2","G3","G4","G1")
+                    every { dao.getQuizQuestions(1) }returns dao.getQuizQuestions(1)
+                       assertEquals(dao.getQuizQuestions(1), dao.getQuizQuestions(1))
+                }
+         @Test
+            fun `test that getQuizByName function returns quiz`() {
+                val database = mockk<AppDatabase>()
+                val dao = mockk<QuizDao>()
+                val application = mockk<Application>()
+                val appModule = mockk<AppModule>()
+            //quiz
+                val quiz1 = Quiz("Joe","Best Quize","people Around the World","General",23)
+                //Questions
+                    val questions1 : Questions = Questions(1,"What's your group", "G1","G2","G3","G4","G1")
+
+                every { dao.getMyQuizzes(23) }returns dao.getMyQuizzes(23)
+                    assertEquals(dao.getMyQuizzes(23), dao.getMyQuizzes(23))
+
+            }
+        @Test
+            fun `test that getQuizBy Id function returns the right quiz`() {
+            val database = mockk<AppDatabase>()
+            val dao = mockk<QuizDao>()
+            val application = mockk<Application>()
+            val appModule = mockk<AppModule>()
+            val questions1: Questions =
+                Questions(1, "What's your group", "G1", "G2", "G3", "G4", "G1")
+
+            every { dao.getQuizQuestions(1) } returns listOf(questions1)
+            assertEquals(listOf(questions1), dao.getQuizQuestions(1))
+            verify { dao.getQuizQuestions(1) }
+            verify(exactly = 1) { dao.getQuizQuestions(23) }
+        }
+        @Test
+        fun `test that calling dao returns dao`() {
+            val database = mockk<AppDatabase>()
+            every { database.quizDao() } returns dao
+            val application = mockk<Application>()
+            every { application.applicationContext } returns application
+        assertEquals(dao, database.quizDao())
+        verify { database.quizDao() }
+        verify(exactly = 1) { database.quizDao() }
 
     }
 }
--- app/src/test/java/com/example/riddler/data/di/AppModuleTest.kt
+++ app/src/test/java/com/example/riddler/data/di/AppModuleTest.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import com.example.riddler.RetroApiInterface
 import com.example.riddler.data.dao.QuizDao
 import com.example.riddler.data.database.AppDatabase
+import com.example.riddler.data.repo.FirestoreRepository
 import com.example.riddler.ui.viewmodel.just
 import io.mockk.*
 import org.junit.Assert.*
@@ -32,30 +33,45 @@ class AppModuleTest {
 
     @Test
     //getAppDB()
-    fun getAppDB() {
+    fun `function to test if getAppDB returrns database`() {
         val dao = mockk<QuizDao>()
         val api = mockk<RetroApiInterface>()
         val database = mockk<AppDatabase>()
         val application = mockk<Application>()
 
         coEvery { appModule.getAppDB(application) } returns database
+        val result = appModule.getAppDB(application)
+        assertEquals(database, result)
     }
 
     @Test
     //getQuizDao()
-    fun getQuizDao() {
+    fun `Funtions to test is getQuizDao returns dao`() {
         val database = mockk<AppDatabase>()
         val dao = mockk<QuizDao>()
         coEvery { appModule.getQuizDao(database) } returns dao
+        val result = appModule.getQuizDao(database)
+        assertEquals(dao, result)
+
 
     }
 
     @Test
     //getRetroApi()
-    fun getRetroApi() {
+    fun `function to test if getRetroApi() will return retro api`() {
         val api = mockk<RetroApiInterface>()
         //verify { appModule.getRetroApi() }
         coEvery { appModule.getRetroApi() } returns api
+        val result = appModule.getRetroApi()
+        assertEquals(api, result)
 
+    }
+    @Test
+    //getFirestoreRepository()
+    fun `function to test if getFirestoreRepository() will return firestore repository`() {
+        val repository = mockk<FirestoreRepository>()
+        coEvery { appModule.getFirestoreRepository() } returns repository
+        val result = appModule.getFirestoreRepository()
+        assertEquals(repository, result)
     }
 }
