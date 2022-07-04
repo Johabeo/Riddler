9e6c427d26c7b72cf86ac274b24eb9dd62af36b6
David Odhiambo <odhiambodave@live.com>
2022-07-03 21:09:09 -0700

    lobby test for players

---
+++ app/src/test/java/com/example/riddler/data/model/LobbyTest.kt
@@ -0,0 +1,39 @@
+package com.example.riddler.data.model
+
+import io.mockk.mockk
+import org.junit.Assert.*
+import org.junit.Test
+
+class LobbyTest{
+    @Test
+    fun testLobby(){
+        val host = "host"
+        val pin = "pin"
+        val size = 2979
+        val gameStarted = false
+        val player = mockk<LobbyPlayers>()
+        val players = listOf(player)
+        val underTest = Lobby(
+            host = host,
+            pin = pin,
+            size = size,
+            gameStarted = gameStarted,
+            players = players,
+        )
+        val lobby = Lobby("test", "test", 4, false, listOf(player))
+        assertEquals("test", lobby.host)
+        assertEquals("test", lobby.pin)
+        assertEquals("test", lobby.host)
+    }
+
+    @Test
+    fun testLobbyPlayers(){
+        val name = "name"
+        val underTest = LobbyPlayers(1, name)
+        assertEquals("name", underTest.playerName)
+        assertEquals(1, underTest.playerId)
+
+    }
+
+
+}
