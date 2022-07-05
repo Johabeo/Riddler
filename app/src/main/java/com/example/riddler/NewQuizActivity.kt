9376f38905b69c48fca5281c59824220ef5eac1c
jshev <jshev150@gmail.com>
2022-07-05 10:05:53 -0400

    added form

---
+++ app/src/main/java/com/example/riddler/NewQuizActivity.kt
@@ -0,0 +1,13 @@
+package com.example.riddler
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewQuizActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_quiz)
+
+
+    }
+}
